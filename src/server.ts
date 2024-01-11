3e5f3477d49126ccf7934fc217f1dc03fd39e167
Marcelo Abbadia <marceloabbadia@Marcelos-MacBook-Pro.local>
2024-01-11 16:57:14 -0300

    Configuration adjustments

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { env } from './env'
 
 
 app.listen({
-  host: ('RENDER' in process.env) ? '0.0.0.0' : 'localhost',
+  //host: ('RENDER' in process.env) ? '0.0.0.0' : 'localhost',
   port: env.PORT
 
 }).then(()=>{
