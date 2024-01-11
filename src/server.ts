0471116bb652077ae077cb8eda1567d6604c46ae
Marcelo Abbadia <marceloabbadia@Marcelos-MacBook-Pro.local>
2024-01-11 16:37:21 -0300

    Deployment setup completed

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import { env } from './env'
 
 
 app.listen({
+  host: ('RENDER' in process.env) ? '0.0.0.0' : 'localhost',
   port: env.PORT
+
 }).then(()=>{
+
+  
   console.log('HTTP Server Running')
 })
